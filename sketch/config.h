95638f23872c9c208a76490cbba485a560658df0
Matt Richardson <matt.richardson@octopus.com>
2022-01-14 21:35:37 +1100

    Add initial info for the OTA updater

--- sketch/config.h
+++ sketch/config.h
@@ -3,11 +3,17 @@
 
 #define WLAN_SSID  "${{ secrets.WLAN_SSID }}"
 #define WLAN_PASSWD "${{ secrets.WLAN_PASSWD }}"
+
 const int SLEEPTIME = 30e7; //300 seconds
 
 //io.adafruit.com
 #define IO_USERNAME  "${{ secrets.IO_USERNAME }}"
 #define IO_KEY       "${{ secrets.IO_KEY }}"
 #define IO_FEEDNAME  "${{ secrets.IO_FEEDNAME }}"
+
+//auth for the ota endpoint
+#define OTA_ENDPOINT "${{ secrets.OTA_ENDPOINT }}"
+#define OTA_USERNAME "${{ secrets.OTA_USERNAME }}"
+#define OTA_PASSWORD "${{ secrets.OTA_PASSWORD }}"
 
 #endif
