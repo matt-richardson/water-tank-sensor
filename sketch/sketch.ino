29991c7b6528489ebad0a1b8c4c8a24ae2f14795
Matt Richardson <matt.richardson@octopus.com>
2022-01-17 21:21:38 +1100

    Hacky attempt at structured logging

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -113,24 +113,38 @@ float average (float * array, int len)
   return  ((float) sum) / nonZeroValues;
 }
 
-void log(String message)
+void log(String message, String messageTemplate = "", String value1Name = "", String value1 = "", String value2Name = "", String value2 = "")
 {
   Serial.println(message);
 
-  HTTPClient http;
-  WiFiClientSecure wifiClient;
-  wifiClient.setInsecure(); //todo: fix so it does proper validation
-  http.begin(wifiClient, SEQ_URL "/api/events/raw");
-  http.addHeader("Content-Type", "application/vnd.serilog.clef");
-  http.addHeader("X-Seq-ApiKey", SEQ_API_KEY);
-  String postData = "{\"@t\":\"" + dateTime(ISO8601) + "\", \"@m\": \"" + message + "\", \"Application\": \"WaterTankSensor\", \"Environment\": \"Production\", \"Version\": \"" + VERSION_NUMBER + "\"}";
-  Serial.print("Sending data: ");
-  Serial.println(postData);
-  auto httpCode = http.POST(postData);
-  Serial.println(httpCode); //Print HTTP return code
-  String payload = http.getString();
-  Serial.println(payload); //Print request response payload
-  http.end(); //Close connection
+  if (messageTemplate != "") {
+      HTTPClient http;
+      WiFiClientSecure wifiClient;
+      wifiClient.setInsecure(); //todo: fix so it does proper validation
+      http.begin(wifiClient, SEQ_URL "/api/events/raw");
+      http.addHeader("Content-Type", "application/vnd.serilog.clef");
+      http.addHeader("X-Seq-ApiKey", SEQ_API_KEY);
+      String postData = String("{") +
+                        "\"@t\":\"" + dateTime(ISO8601) + "\"," +
+                        "\"@m\": \"" + message + "\"," +
+                        "\"Application\": \"WaterTankSensor\"," +
+                        "\"Environment\": \"Production\"," +
+                        "\"Version\": \"" + VERSION_NUMBER + "\"";
+      if (value1Name != "") {
+          postData = postData + ",\"" + value1Name + "\": \"" + value1 + "\"";
+      }
+      if (value1Name != "") {
+          postData = postData + ",\"" + value2Name + "\": \"" + value2 + "\"";
+      }
+      postData = postData + "}";
+      Serial.print("Sending data: ");
+      Serial.println(postData);
+      auto httpCode = http.POST(postData);
+      Serial.println(httpCode); //Print HTTP return code
+      String payload = http.getString();
+      Serial.println(payload); //Print request response payload
+      http.end(); //Close connection
+  }
 }
 
 void setup()
@@ -140,17 +154,17 @@ void setup()
 
   ConnectWifi();
 
-	waitForSync(); //wait for ntp sync
+  waitForSync(); //wait for ntp sync
 
   Serial.println();
 
-  log("WLAN_SSID: " WLAN_SSID);
-  log("SEQ_URL: " SEQ_URL);
-  log("IO_USERNAME: " IO_USERNAME);
-  log("IO_FEEDNAME: " IO_FEEDNAME);
-  log("OTA_ENDPOINT: " OTA_ENDPOINT);
-  log("OTA_USERNAME: " OTA_USERNAME);
-  log("VERSION_NUMBER: " VERSION_NUMBER);
+  log("WLAN_SSID: " WLAN_SSID, "Config entry {ConfigName} = {ConfigValue}", "ConfigName", "WLAN_SSID", "ConfigValue", WLAN_SSID);
+  log("SEQ_URL: " SEQ_URL, "Config entry {ConfigName} = {ConfigValue}", "ConfigName", "SEQ_URL", "ConfigValue", SEQ_URL);
+  log("IO_USERNAME: " IO_USERNAME, "Config entry {ConfigName} = {ConfigValue}", "ConfigName", "IO_USERNAME", "ConfigValue", IO_USERNAME);
+  log("IO_FEEDNAME: " IO_FEEDNAME, "Config entry {ConfigName} = {ConfigValue}", "ConfigName", "IO_FEEDNAME", "ConfigValue", IO_FEEDNAME);
+  log("OTA_ENDPOINT: " OTA_ENDPOINT, "Config entry {ConfigName} = {ConfigValue}", "ConfigName", "OTA_ENDPOINT", "ConfigValue", OTA_ENDPOINT);
+  log("OTA_USERNAME: " OTA_USERNAME, "Config entry {ConfigName} = {ConfigValue}", "ConfigName", "OTA_USERNAME", "ConfigValue", OTA_USERNAME);
+  log("VERSION_NUMBER: " VERSION_NUMBER, "Config entry {ConfigName} = {ConfigValue}", "ConfigName", "VERSION_NUMBER", "ConfigValue", VERSION_NUMBER);
 
   Serial.println();
 
@@ -160,11 +174,11 @@ void setup()
     readings[i] = hc.dist();
     char buffer[60];
     sprintf(buffer, "Water level (reading %d) is %f cm from the sensor", i, readings[i]);
-    log(buffer); // Print in centimeters the value from the sensor
+    log(buffer, "Water level (reading {ReadingNumber}) is {ReadingValue} cm from the sensor", "ReadingNumber", String(i), "ReadingValue", String(readings[i])); // Print in centimeters the value from the sensor
     delay(250);
   }
   float averageReading = average(readings, 10);
-  log("Average reading is " + String(averageReading));
+  log("Average reading is " + String(averageReading), "Average reading is {AverageValue} cm from the sensor", "AverageValue", String(averageReading));
 
   float distanceFromBottomOfTank = TANK_SENSOR_HEIGHT_IN_CM - averageReading;
   float percentageFull = (distanceFromBottomOfTank / TANK_OVERFLOW_HEIGHT_IN_CM) * 100;
@@ -172,7 +186,7 @@ void setup()
   SendData(percentageFull);
 
   CheckForUpdate();
-  log("Going into deep sleep mode for " + String(SLEEPTIME / 1000000) + String(" seconds"));
+  log("Going into deep sleep mode for " + String(SLEEPTIME / 1000000) + String(" seconds"), "Going into deep sleep mode for {SleepTime} seconds", "SleepTime", String(SLEEPTIME / 1000000));
   ESP.deepSleep( SLEEPTIME, WAKE_RF_DISABLED );
 }
 
